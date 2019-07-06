294a26468da1b9e3528dba0a7be67ffb0751fc03
Brett A. Jory <brett.austin.jory@gmail.com>
2019-07-06 13:46:02 -0700

    fold structure creation complete

--- sass-me/index.js
+++ sass-me/index.js
@@ -1,15 +1,45 @@
 const fs = require('fs');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-function buildTree(startPath) {
-  fs.readdir(startPath, (err, entries) => {
-    console.log(entries);
-    entries.forEach(file => {
-      const path = `${startPath}/${file}`;
-      if (fs.lstatSync(path).isDirectory()) {
-        buildTree(path);
-      }
-    });
+const sassImports = `
+  @import 'abstracts/_functions';
+  @import 'abstracts/_mixins';
+  @import 'abstracts/_variables';
+
+  @import 'base/_animations';
+  @import 'base/_base';
+  @import 'base/_typography';
+  @import 'base/_utilities';
+
+  @import 'layouts/_footer';
+  @import 'layouts/_header';
+  @import 'layouts_navigation';
+
+  @import 'pages/_home';
+  `
+
+function createFileStructure() {
+  console.log("Creating SASS file structure");
+  fs.mkdirSync("./sass-project", err => {
+    if (err) { throw err; }
+  });
+  fs.mkdirSync("./sass-project/abstracts", {recursive: true}, err => {
+    if (err) { throw err; }
+  });
+  fs.mkdirSync("./sass-project/base", {recursive: true}, err => {
+    if (err) { throw err; }
+  });
+  fs.mkdirSync("./sass-project/layouts", {recursive: true}, err => {
+    if (err) { throw err; }
+  });
+  fs.mkdirSync("./sass-project/pages", {recursive: true}, err => {
+    if (err) { throw err; }
   });
 }
 
-buildTree('/home/baj');
+createFileStructure();
+
+process.exit();
